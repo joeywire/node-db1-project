eea6f6106678673c85354ddc557fe03eee7d8d89
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-07 16:55:15 -0600

    Crusin 1/2 of endpoints build and working

--- api/accounts/account-model.js
+++ api/accounts/account-model.js
@@ -3,5 +3,16 @@ const db = require("../../data/dbConfig");
 module.exports = {
     getAll() {
         return db('accounts');
+    }, 
+    getById(id) {
+        // .first() will return the first match - we know the id is unique so this sends back 
+        // a single object vs. a array with a single obj init. 
+        return db('accounts').where('id', id).first();
+    }, 
+    create(post) {
+        return db('accounts').insert(post)
+            .then(([id]) => {
+                return db('accounts').where('id', id).first();
+            })
     }
 };
--- api/accounts/account-router.js
+++ api/accounts/account-router.js
@@ -10,6 +10,27 @@ router.get('/', async (req, res) => {
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
-})
+});
+
+router.get('/:id', async (req,res) => {
+    try {
+        const { id } = req.params; 
+        const data = await Account.getById(id);
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    const post = req.body;
+    try { 
+        const freshAcct = await Account.create(post); 
+        res.status(200).json(freshAcct); 
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 
 module.exports = router; 
