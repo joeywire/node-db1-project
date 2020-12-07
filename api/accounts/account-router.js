2948e99e4bcaae563156613b9d6042a8687a4f3e
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-07 16:31:33 -0600

    First endpoint up and running we're querieing ... but not spelling

--- api/accounts/account-router.js
+++ api/accounts/account-router.js
@@ -3,5 +3,13 @@ const Account = require('./account-model');
 
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    try {
+        const data = await Account.getAll();
+        res.status(200).json(data); 
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+})
 
 module.exports = router; 
